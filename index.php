0ccdd9c6b9198ee1e187d9c56a491fbbfd5b36b3
Hub-Mo <Mohamed.elja@hotmail.com>
2022-07-01 10:46:55 +0200

    added the files needed

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="index.php" method="post">
+    <input type="text" placeholder="pls enter a title">
+    <input type="text" placeholder="pls enter your first-name and last-name here">
+    <input type="text" placeholder="your message comes here" width="250" height="300">
+</form>
+
+</body>
+</html>
